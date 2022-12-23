47c8c8edd73e3d0cd5752bfc59f305e1c2541c6d
h42codes <99951343+h42codes@users.noreply.github.com>
2022-12-23 18:15:11 +0900

    Edit dummyList content

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -6,35 +6,40 @@ const dummyList = [
   {
     id: 1,
     author: "HJ",
-    content: "Hello!",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ante in nibh mauris cursus mattis. Sit amet facilisis magna etiam tempor orci eu.",
     score: 5,
     created_date: new Date().getTime(),
   },
   {
     id: 2,
     author: "HJ",
-    content: "Hello!",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus urna. Arcu ac tortor dignissim convallis aenean.",
     score: 5,
     created_date: new Date().getTime(),
   },
   {
     id: 3,
     author: "HJ",
-    content: "New Year!",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus urna. Arcu ac tortor dignissim convallis aenean.",
     score: 2,
     created_date: new Date().getTime(),
   },
   {
     id: 4,
     author: "HJ",
-    content: "Holiday!",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus urna. Arcu ac tortor dignissim convallis aenean.",
     score: 5,
     created_date: new Date().getTime(),
   },
   {
     id: 5,
     author: "HJ",
-    content: "Bye!",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus urna. Arcu ac tortor dignissim convallis aenean.",
     score: 4,
     created_date: new Date().getTime(),
   },
