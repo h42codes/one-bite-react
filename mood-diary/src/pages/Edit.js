d9020d4e720fdf8130b1d60e703cb4c0381330a4
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-12 23:52:42 +0900

    Add useSearchParams hook

--- mood-diary/src/pages/Edit.js
+++ mood-diary/src/pages/Edit.js
@@ -1,8 +1,19 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+  console.log(id, mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>This is Edit section</p>
+      <button onClick={() => setSearchParams({ who: "me" })}>
+        Change Query String
+      </button>
     </div>
   );
 };
