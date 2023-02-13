fcb2c81d8389435ced4db0632a37ed622106c1b6
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-14 00:13:32 +0900

    Add DiaryItem info section

--- mood-diary/src/components/DiaryItem.js
+++ mood-diary/src/components/DiaryItem.js
@@ -2,12 +2,19 @@ const DiaryItem = ({ id, mood, content, date }) => {
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+  console.log(strDate);
+
   return (
     <div className="DiaryItem">
       <div
         className={["mood_img_wrapper", `mood_img_wrapper_${mood}`].join(" ")}
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${mood}.png`} />
+      </div>
+      <div className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
     </div>
   );
