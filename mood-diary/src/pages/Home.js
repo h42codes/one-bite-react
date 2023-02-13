2ef4b73f76331ad638152210990b4416f7becc08
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-13 23:24:42 +0900

    Add useEffect hook to filter current month's data

--- mood-diary/src/pages/Home.js
+++ mood-diary/src/pages/Home.js
@@ -1,10 +1,36 @@
 import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
 
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
   const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()} / ${curDate.getMonth() + 1}`;
+
+  // only show corresponding month's entries
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter(
+          (item) => item.date >= firstDay && item.date <= lastDay
+        )
+      );
+    }
+  }, [diaryList, curDate]);
 
   const increaseMonth = () => {
     setCurDate(
