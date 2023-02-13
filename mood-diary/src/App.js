a2929d9541f0994a08aece1ca2ee729b3817b42e
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-13 17:50:03 +0900

    Remove unused components

--- mood-diary/src/App.js
+++ mood-diary/src/App.js
@@ -6,46 +6,10 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-// COMPONENTS
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
-
 function App() {
-  // const env = process.env;
-  // env.PUBLIC_URL = env.PUBLIC_URL || "";
-
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton text="LEFT" onClick={() => alert("LEFT clicked")} />
-          }
-          rightChild={
-            <MyButton text="RIGHT" onClick={() => alert("RIGHT clicked")} />
-          }
-        ></MyHeader>
-        <h2>App.js</h2>
-        <MyButton
-          text="button"
-          onClick={() => alert("button clicked")}
-          type={"positive"}
-        ></MyButton>
-        <MyButton
-          text="button"
-          onClick={() => alert("button clicked")}
-          type={"negative"}
-        ></MyButton>
-        <MyButton
-          text="button"
-          onClick={() => alert("button clicked")}
-        ></MyButton>
-        {/* <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"} alt="" />
-        <img src={process.env.PUBLIC_URL + "/assets/emotion2.png"} alt="" />
-        <img src={process.env.PUBLIC_URL + "/assets/emotion3.png"} alt="" />
-        <img src={process.env.PUBLIC_URL + "/assets/emotion4.png"} alt="" />
-        <img src={process.env.PUBLIC_URL + "/assets/emotion5.png"} alt="" /> */}
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/new" element={<New />}></Route>
