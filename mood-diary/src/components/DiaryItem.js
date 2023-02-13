b512bddd9f19febd2009f46ac4698f851b54386f
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-14 00:19:22 +0900

    Add DiaryItem Edit button

--- mood-diary/src/components/DiaryItem.js
+++ mood-diary/src/components/DiaryItem.js
@@ -1,20 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
 const DiaryItem = ({ id, mood, content, date }) => {
+  const navigate = useNavigate();
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
   const strDate = new Date(parseInt(date)).toLocaleDateString();
-  console.log(strDate);
+
+  const goToDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goToEdit = () => {
+    navigate(`/edit/${id}`);
+  };
 
   return (
     <div className="DiaryItem">
       <div
+        onClick={goToDetail}
         className={["mood_img_wrapper", `mood_img_wrapper_${mood}`].join(" ")}
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${mood}.png`} />
       </div>
-      <div className="info_wrapper">
+      <div onClick={goToDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
+      <div className="btn_wrapper">
+        <MyButton text={"Edit"} onClick={goToEdit} />
       </div>
     </div>
   );
