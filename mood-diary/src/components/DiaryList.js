ed97c70ad0874b630592b5bb278738f0a2ced043
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-13 23:35:35 +0900

    Add sorting control menu

--- mood-diary/src/components/DiaryList.js
+++ mood-diary/src/components/DiaryList.js
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "latest" },
+  { value: "oldest", name: "oldest" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((item, idx) => (
+        <option key={idx} value={item.value}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+
   return (
     <div>
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
       {diaryList.map((item) => (
         <div key={item.id}>{item.content}</div>
       ))}
