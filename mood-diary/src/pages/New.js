41b30a4407f1262d413639b30b899eb97c525744
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-14 14:43:01 +0900

    Add DiaryEditor component

--- mood-diary/src/pages/New.js
+++ mood-diary/src/pages/New.js
@@ -1,34 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
   return (
     <div>
-      <MyHeader
-        headText={"New Entry"}
-        leftChild={<MyButton text={"< Back"} onClick={() => navigate(-1)} />}
-      />
-      <div>
-        <section>
-          <h4>Today's Date</h4>
-          <div className="input-box">
-            <input
-              className="input-date"
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
