85f15878f2c57fcefe471f5e2681cf117d00efa3
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-14 14:39:51 +0900

    Add date section

--- mood-diary/src/pages/New.js
+++ mood-diary/src/pages/New.js
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
   const navigate = useNavigate();
   return (
     <div>
@@ -11,6 +16,19 @@ const New = () => {
         headText={"New Entry"}
         leftChild={<MyButton text={"< Back"} onClick={() => navigate(-1)} />}
       />
+      <div>
+        <section>
+          <h4>Today's Date</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
