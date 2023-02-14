f43b43a34d04cc74f51a9405cbdcfb26b382b0f0
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-14 15:18:03 +0900

    Add styling to MoodItem

--- mood-diary/src/components/MoodItem.js
+++ mood-diary/src/components/MoodItem.js
@@ -1,6 +1,12 @@
-const MoodItem = ({ mood_id, mood_img, mood_desc }) => {
+const MoodItem = ({ mood_id, mood_img, mood_desc, onClick, isSelected }) => {
   return (
-    <div className="MoodItem">
+    <div
+      className={[
+        "MoodItem",
+        isSelected ? `MoodItem_on_${mood_id}` : `MoodItem_off`,
+      ].join(" ")}
+      onClick={() => onClick(mood_id)}
+    >
       <img src={mood_img} />
       <span>{mood_desc}</span>
     </div>
